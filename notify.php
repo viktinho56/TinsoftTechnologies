8fda4c804d996dc6de59ce5df229b08058453f7e
viktinho56 <viktinho56@gmail.com>
2022-03-25 01:05:27 +0100

    Update notify.php

--- notify.php
+++ notify.php
@@ -1,4 +1,5 @@
 <?php
+ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL); 
   /**
   * Requires the "PHP Email Form" library
   * The "PHP Email Form" library is available only in the pro version of the template
